2d22c2476cb208f82d4b65d7d3092716e0a0a08b
shubhamkarekar <shubhamkarekar191811@gmail.com>
2024-08-21 16:40:54 +0100

    new

---
+++ FavouritePlayers+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  FavouritePlayers+CoreDataClass.swift
+//  Ios_Assignment2_Remake
+//
+//  Created by Shubham Karekar on 17/04/2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(FavouritePlayers)
+public class FavouritePlayers: NSManagedObject {
+
+}
